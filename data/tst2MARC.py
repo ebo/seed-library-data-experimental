130c930b3a980eb4d6e8f02defdab469deb565a4
EBo <ebo.2112@gmail.com>
2026-06-23 12:24:44 -0400

    added proper logging (including log to file). added proper command line arguments. added JSON payload example.

--- data/tst2MARC.py
+++ data/tst2MARC.py
@@ -3,6 +3,14 @@
 # FIXME: this is a test of some infrastructure to see how it
 #   behaves. It is intended for experimental purposes only.
 
+###############################################################################
+# this is a collection of class:variable/value substitutions to MARC21 tags.
+#
+# FIXME: I probably will not be able to use a simple class/dict for
+#        entries that can take multiple copies of a particular tag.
+#        Need to refactor code to support.
+###############################################################################
+
 import os,sys
 import pymarc
 from datetime import datetime
@@ -13,20 +21,23 @@ if sys.version_info < (3, 14):
     import uuid_utils as uuid
 
 ###############################################################################
-# this is a collection of class:variable/value substitutions to MARC21 tags.
-#
-# FIXME: I probably will not be able to use a simple class/dict for
-#        entries that can take multiple copies of a particular tag.
-#        Need to refactor code to support.
+# logging handlers
+###############################################################################
+import logging
+logger = logging.getLogger(__name__)
+
 ###############################################################################
 
 # The generic base class.  Most functions have been refactored to
 # trigger off of the ctype variable, which is set in the derived
 # classes below.
 #
+
+###############################################################################
 # This generic base class is driven by a combination of the type of
-# object being worked on (ctype), and the MARC21 specific mappings (in
-# a provided CSV file).
+# object being worked on (ctype), and the MARC21 specific mappings
+# (from an external CSV file).
+###############################################################################
 
 # FIXME: this is a Teriable name.  Need a better one.
 class Taxonomy:
@@ -59,14 +70,14 @@ class Taxonomy:
         elif 0 == dl:
             # No name was found. skip and warn about bad name
             # FIXME: convert to proper logging
-            print(f"Warning: variable '{var}' not found.")
+            logger.warn(f" variable '{var}' not found.")
         else:
             # Multiple map entries found.  Post error to repair mappings.
             # FIXME: convert to proper logging
-            print(f"Error: variable '{var}' returns more than one mapping.")
-            print(f"       repair mappings file entries:")
+            logger.exception(f" variable '{var}' returns more than one mapping.")
+            logger.exception(f"     repair mappings file entries:")
             for d in dct:
-                print(f"    {d}")
+                logger.exception(f"    {d}")
 
         return
 
@@ -101,7 +112,7 @@ class Taxonomy:
 
         # iterate through the tags and find all set
         for t in sorted(set([itm['tag'] for itm in mappings])):
-            print(f"processing tag '{t}':")
+            logger.debug(f" processing field: '{t}':")
             nsubfields = []
             for itm in list(vars(self)):
                 try:
@@ -111,13 +122,7 @@ class Taxonomy:
                 
                 if dt == t:
                     v = getattr(self,itm)
-                    #print(f" ... {dt,ds,dv} = {v}")
                     nsubfields.append(Subfield(code=ds, value=v))
-                    #print(f"tag={dt}  type={type(dt)}")
-                    #print(f"subtag={ds}  type={type(ds)}")
-                    #print(f"var={dv}  type={type(dv)}")
-                    #print(f"value={v}  type={type(v)}")
-                    #print("")
                     
             record.add_field(
                 Field(
@@ -144,7 +149,7 @@ class Taxonomy:
             writer.write(record)
         
             marc_string = mstream.getvalue().decode('utf-8')
-            print(marc_string)
+            logger.debug(marc_string)
             writer.close()
             return marc_string
         else:
@@ -177,13 +182,13 @@ class Taxonomy:
     # FIXME: need to migrate error messages into proper logging.
     def check_required(self):
         ret = True
-        required = [itm["var"] for itm in mappings if itm["required"]==True and itm["class"]==self.ctype]
+        required = [self.__gen_name__(itm) for itm in mappings if itm["required"]=="True" and itm["class"]==self.ctype]
         my_vars = list(vars(self))
-
+        
         for k in required:
             if k not in my_vars:
                 ret = False
-                print(f"Error: Required variable '{k}' not in '{self.ctype}' object")
+                logger.error(f" Required variable '{k}' not in '{self.ctype}' object")
         return ret
 
 # the Species class covers the biographic analogy of species <=> author.
@@ -202,11 +207,43 @@ class Collection (Taxonomy):
 ###############################################################################
 
 if __name__ == "__main__":
-    map_file = "mappings_v0.1.csv"
+    import argparse
+
+    prog = os.path.basename(__file__) # os.path.basename(sys.executable)
+    
+    parser = argparse.ArgumentParser(prog=prog, description="Convert a Species or Seed Collection to a MARC21 record (experimental testing).")
+
+    parser.add_argument("-i","--infile", type=str, required=True,
+                        help="The input species/collection file")
+    parser.add_argument("-m","--mapfile", type=str, required=False,
+                        default="./mappings_v0.1.csv",
+                        help="The MARC21 mappings file (default: %(default)s)")
+    parser.add_argument("-L","--logfile", type=str, required=False,
+                        default=None,
+                        help="Output log filename (default: %(default)s)")
+   
+    parser.add_argument("-l", "--log", default='WARNING', required=False,
+                        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+                        help='Set the logging level (default: %(default)s)')
+    
+    args = parser.parse_args()
+
+    # set the log levels, and logging to external file if desired.
+    FORMAT = '%(asctime)s - %(levelname)s - [%(filename)s:%(lineno)d] - %(message)s'
+    logging.basicConfig(level=args.log,handlers=[logging.StreamHandler()],
+                        format=FORMAT)
+    if args.logfile is not None:
+        file_handler = logging.FileHandler(args.logfile, mode="w", encoding="utf-8")
+        log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        formatter = logging.Formatter(fmt=FORMAT)
+        
+        file_handler.setLevel(args.log)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
 
     # convert the CSV table into a mappings array of dictionarys.
     global mappings
-    with open(map_file, mode='r') as mp:
+    with open(args.mapfile, mode='r') as mp:
         import csv
         
         # Create a DictReader object (ignoring any comments)
@@ -215,106 +252,20 @@ if __name__ == "__main__":
         # Convert the reader object into a list of dictionaries
         mappings = [row for row in reader]
 
-    if False:
-        # debug write: the map file 
-        for itm in mappings:
-            for k in itm:
-                print(f"{k}: {itm[k]}")
-            print("")
-    
-    if True:
-        # simple test of a species I am interested in
-        s = Species()
-        s.set("full_name","Sanguinaria canadensis L.")
-        s.set("common_name","bloodroot")
-        #s.set("species_id","urn:usda:saca13")
-        s.set("species_id","saca13")
-        s.set("source","plantsdb")
-        s.set("link","https://www.loc.gov/standards/sourcelist/taxonomic.html")
-        s.set("auxilary_info","This is a JSON load test.")
-        s.set("auxilary_data",{"name":"test","var":"woof"})
+    # simple test of a species I am interested in
+    s = Species()
+    s.set("full_name","Sanguinaria canadensis L.")
+    s.set("common_name","bloodroot")
+    #s.set("species_id","urn:usda:saca13")
+    s.set("species_id","saca13")
+    s.set("source","plantsdb")
+    s.set("link","https://www.loc.gov/standards/sourcelist/taxonomic.html")
+    s.set("auxilary_info","This is a JSON load test.")
+    s.set("auxilary_data",{"name":"test","var":"woof"})
 
-        # check to make sure that all required variables are set
-        s.check_required()
+    # check to make sure that all required variables are set
+    logger.info(f" check all required fields set: {s.check_required()}")
+
+    print("")
+    print(s.MARC21())
         
-    if False:
-        print(s)
-        print(repr(s))
-
-        print("check:",s.check_required())
-
-    if True:
-        ret = s.MARC21()
-        print("record:")
-        print(ret)
-        
-    if False:
-        from pymarc import Record, Field, Subfield, MARCWriter
-
-        # 1. Initialize a new blank MARC record
-        record = Record()
-
-        # 2. Add control fields (tags under 010 do not use indicators
-        #    or subfields)
-        record.add_field(
-            Field(tag='001', data='ocm01234567')
-        )
-
-        
-        # 3. Add variable data fields using Subfield objects
-        # Example: ISBN (020)
-        record.add_field(
-            Field(
-                tag='020',
-                indicators=[' ', ' '],
-                subfields=[
-                    Subfield(code='a', value='9780135957059')
-                ]
-            )
-        )
-
-        # Example: Author Main Entry (100)
-        record.add_field(
-            Field(
-                tag='100',
-                indicators=['1', ' '],
-                subfields=[
-                    Subfield(code='a', value='Thomas, David,'),
-                    Subfield(code='e', value='author.')
-                ]
-            )
-        )
-        
-        # Example: Title Statement (245)
-        record.add_field(
-            Field(
-                tag='245',
-                indicators=['1', '4'],  # Ind 2 = 4 ignores "The " when indexing/sorting
-                subfields=[
-                    Subfield(code='a', value='The pragmatic programmer : '),
-                    Subfield(code='b', value='your journey to mastery / '),
-                    Subfield(code='c', value='David Thomas, Andrew Hunt.')
-                ]
-            )
-        )
-
-        # Example: Publication Information (264 / 260)
-        record.add_field(
-            Field(
-                tag='264',
-                indicators=[' ', '1'],
-                subfields=[
-                    Subfield(code='a', value='Boston : '),
-                    Subfield(code='b', value='Addison-Wesley, '),
-                    Subfield(code='c', value='2020.')
-                ]
-            )
-        )
-
-        # 4. Save the record to a binary MARC (.mrc) file
-        with open('output_records.mrc', 'wb') as file_handler:
-            writer = MARCWriter(file_handler)
-            writer.write(record)
-            writer.close()
-            
-        print("MARC record successfully created and saved.")
